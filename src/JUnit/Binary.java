ffa42f32c593e17052873f89b03c8ef76dc243f5
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-05-23 15:59:21 +0530

    updated binary related problems

---
+++ src/JUnit/Binary.java
@@ -0,0 +1,23 @@
+package JUnit;
+
+import java.util.Scanner;
+
+public class Binary {
+    public static void toBinary(int number){
+        int binary[] = new int[40];
+        int index = 0;
+        while(number > 0){
+            binary[index++] = number%2;
+            number = number/2;
+        }
+        for(int i = index-1;i >= 0;i--){
+            System.out.print(binary[i]);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        toBinary(number);
+    }
+}
